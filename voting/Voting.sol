0ea77ca0935352312c32011c66be3dcb8c478597
damienh972 <helo.dams@gmail.com>
2020-11-01 23:12:57 +0100

    addition of exercices

---
+++ voting/Voting.sol
@@ -0,0 +1,44 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.11;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Voting is Ownable{
+    
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+    
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    
+    uint8 winningProposalId;
+    mapping (address  => bool) public whitelist;
+    
+    event VoterRegistered(address voterAddress);
+    event ProposalsRegistrationStarted();
+    event ProposalsRegistrationEnded();
+    event ProposalRegistered(uint proposalId);
+    event VotingSessionStarted();
+    event VotingSessionEnded();
+    event Voted (address voter, uint proposalId);
+    event VotesTallied();
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    
+    
+    
+}
